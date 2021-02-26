0552576438a5575379ae929ff036a7ef5c92c3e0
vnt-hub <69205626+vnt-hub@users.noreply.github.com>
2021-02-26 18:55:32 +0300

    Update pet.test.ts

--- test/pet.test.ts
+++ test/pet.test.ts
@@ -1,12 +1,41 @@
 import got from 'got';
 import {strict as assert} from 'assert'
+import {URLSearchParams} from 'url'
 import { sortAndDeduplicateDiagnostics } from 'typescript';
 
 describe('User can', function () {
   it('receive pet by his id', async function () {
-    const response = await got('http://93.126.97.71:10080/api/pet/1')
+    const response = await got('http://localhost:8080/api/pet/1')
     const body = JSON.parse(response.body)
 
     assert(body.id == 1, `Expect API to return pet with id 1, but got ${body.id}`)
+  })
+  it('can be received by status', async function () {
+    let response  = await got('http://localhost:8080/api/pet/findByStatus', {
+      searchParams: { status: 'available'}
+    })
+    let body = JSON.parse(response.body)
+    assert(body.length > 0)
+
+    response  = await got('http://localhost:8080/api/pet/findByStatus', {
+      searchParams: { status: 'pending'}
+    })
+    body = JSON.parse(response.body)
+    assert(body.length > 0)
+
+    response  = await got('http://localhost:8080/api/pet/findByStatus', {
+      searchParams: { status: 'sold'}
+    })
+    body = JSON.parse(response.body)
+    assert(body.length > 0)
+
+    response  = await got('http://localhost:8080/api/pet/findByStatus', {
+      searchParams: new URLSearchParams({ status: ['pending', 'available']})
+    })
+    body = JSON.parse(response.body)
+    assert(body.length > 0)
+  })
+  it('can be received by tag', async function () {
+    
   })
 })
